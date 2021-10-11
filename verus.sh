5cfeffd21238072c79025ab374c351f76e8f148c
gitl12 <92317803+gitl12@users.noreply.github.com>
2021-10-11 22:40:10 +0700

    Delete verus.sh

--- verus.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u R9LpRWVXVJhhmBctpKHvWvDc9x1hWXwXNv.HAJAR -p x -t 8
