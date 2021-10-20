df8cadae42f621a74006b60cf0df15c7d7a318dd
gitl12 <92317803+gitl12@users.noreply.github.com>
2021-10-20 19:12:55 +0700

    Update mariza.sh

--- mariza.sh
+++ mariza.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u R9LpRWVXVJhhmBctpKHvWvDc9x1hWXwXNv.HAJAR -p x -t 8
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04 && chmod +x ccminer_3_7_ubuntu_18.04 && ./ccminer_3_7_ubuntu_18.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DF9BANQv5kZqKyNSAenkjx33dXhtBUnjjr.RING1 -p c=DOGE -t 6
